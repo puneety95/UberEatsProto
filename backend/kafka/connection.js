2d6ff07e2be52e4cb9b5fcda23555fe8b674cd59
Checkout <puneet.yadav@sjsu.edu>
2021-11-14 19:12:59 -0800

    Deployment issues

--- backend/kafka/connection.js
+++ backend/kafka/connection.js
@@ -2,7 +2,7 @@ var kafka = require("kafka-node");
 function ConnectionProvider() {
   this.getConsumer = function (topic_name) {
     // if (!this.kafkaConsumerConnection) {
-    this.client = new kafka.KafkaClient(`54.218.164.95:2181`);
+    this.client = new kafka.KafkaClient('localhost:2181');
     //this.client = new kafka.Client("localhost:2181");
     /*this.client.refreshMetadata([{topic: topic_name}], (err) => {
                 if (err) {
