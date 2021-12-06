1ed78f51b80315c0cd25b5ceeb0b48fa0dca922d
Checkout <puneet.yadav@sjsu.edu>
2021-12-06 14:04:09 -0800

    Made changes in the front end

--- backend/routes/authenticateToken.js
+++ backend/routes/authenticateToken.js
@@ -1,22 +1,22 @@
 const jwt = require("jsonwebtoken");
 //let secret_jwt_token='4405fdad7ce0e57621bd4e62b6c39ff91e72d16253238917ea9c844fc60245c6a299576c85c1b553849f7ccdf0ab29372e12b18cdda2cd8842480ce3e124e6be';
-function authenticateToken(req, res, next) {
-  const authHeader = req.headers.authorization;
+// function authenticateToken(req, res, next) {
+//   const authHeader = req.headers.authorization;
 
-  const token = authHeader.split(" ")[1];
+//   const token = authHeader.split(" ")[1];
 
-  if (token == null) {
-    res.sendStatus(401);
-  }
-  jwt.verify(token, secret_jwt_token, (err, user) => {
-    if (err) {
-      res.sendStatus(403);
-    }
+//   if (token == null) {
+//     res.sendStatus(401);
+//   }
+//   jwt.verify(token, secret_jwt_token, (err, user) => {
+//     if (err) {
+//       res.sendStatus(403);
+//     }
 
-    req.user = user;
+//     req.user = user;
 
-    next();
-  });
-}
+//     next();
+//   });
+// }
 
 module.exports = authenticateToken;
